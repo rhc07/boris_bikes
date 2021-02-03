10d916fe500f092a471ad0e4a4b7501e19237a87
Rorie Clarke <Rorie@MacBook-Pro.local>
2021-02-03 17:46:11 +0000

    Step 11

--- lib/DockingStation_class.rb
+++ lib/DockingStation_class.rb
@@ -1,6 +1,22 @@
 class DockingStation
+attr_reader :bike
+
 
 def release_bike
+ @bike = Bike.new
+end
+
+def dock_bike
+
+end
+end
+
+
+class Bike
+
+def working?
+ return true
 end
 
 end
+
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,21 @@
 require "DockingStation_class.rb"
 
 describe DockingStation do 
-  describe ".release_bike" do
-  	#it { "DockingStation responds_to .release_bike" } 
-  	#expect("DockingStation").to respond_to(:release_bike)
-  	it { is_expected.to respond_to(:release_bike)}
+  it "Object responds to relase_bike" do
+  	expect(DockingStation.new).to respond_to(:release_bike)
+  end
+  it "Gets a bike" do
+    expect(DockingStation.new.release_bike).to be_a(Bike)
+  end
+  it "is working" do
+	expect(DockingStation.new.release_bike.working?).to eq(true)
   end
 end
+
+describe Bike do
+  it "Is bike.working?" do
+    expect(Bike.new).to respond_to(:working?)
+  end
+end
+
+
