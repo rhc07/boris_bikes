9263cf97ed03889c37ef54d75e2713f513267b90
Rorie Clarke <Rorie@MacBook-Pro.local>
2021-02-06 10:32:06 +0000

    Completed step 17

--- lib/DockingStation_class.rb
+++ lib/DockingStation_class.rb
@@ -2,29 +2,35 @@ require "./lib/Bike.rb"
 
 class DockingStation
 
-def initialize
-  @bikes = []
+DEFAULT_CAPACITY = 20
+attr_accessor :capacity
+
+def initialize(capacity = DEFAULT_CAPACITY)
+  	@capacity = capacity
+  	@bikes = []	
 end
 
 def dock_bike(bike)
- fail "docking station full" if full?
- @bikes << bike
+ 	fail "docking station full" if full?
+ 	bikes << bike
 end
 
 def release_bike
- fail "No bikes available" if empty?
- @bikes.pop
+ 	fail "No bikes available" if empty?
+ 	bikes.pop
 end
 
 private
 
+attr_reader :bikes
+
 def full?
-	@bikes.count >= 20
+	bikes.count >= capacity
 end
 
 
 def empty?
-	@bikes.empty?
+	bikes.empty?
 end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,6 +5,24 @@ describe DockingStation do
   it "Object responds to release_bike" do
   	expect(DockingStation.new).to respond_to(:release_bike)
   end
+  it "has a default capacity of 20" do 
+  	docking_station = DockingStation.new
+  	expect(docking_station.capacity).to eq DockingStation::DEFAULT_CAPACITY 
+  end
+  describe "initialization" do
+   # added below
+   	subject {DockingStation.new}
+   	let(:bike) {Bike.new}
+  	it "has a variable capcity" do
+  		described_class::DEFAULT_CAPACITY.times do
+  		 subject.dock_bike(bike)
+  		end
+  		# docking_station = DockingStation.new(50)
+  		# 50.times {docking_station.dock_bike Bike.new }
+		expect {subject.dock_bike Bike.new }.to raise_error 'docking station full'
+  	end
+  end
+
   describe "#release_bike" do 
     it "Gets a bike" do
   	    bike = [Bike.new]
@@ -21,8 +39,14 @@ describe DockingStation do
     end
   describe "#dock_bike" do
   	it "raises an error when capacity is full." do
+  		# subject = DockingStation.new
+  		# DockingStation::DEFAULT_CAPACITY.times do
+  		# subject.dock_bike Bike.new	
+  		# end
   		subject = DockingStation.new
-  		20.times { subject.dock_bike Bike.new }
+  		bike = Bike.new
+  		subject.capacity.times { subject.dock_bike(bike) }
+  		#20.times { subject.dock_bike Bike.new }
   		expect {subject.dock_bike Bike.new }.to raise_error 'docking station full'
   	end
   	# it "docks something" do
